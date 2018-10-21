5aaa7b496af1c60dd999441924ade4536b28d447
Dimitrios Gournias <dimitris.gournias@gmail.com>
2018-10-21 16:29:47 +0300

    folder structure

--- app/src/main/java/com/blue/visitgreece/login/LoginActivity.java
+++ app/src/main/java/com/blue/visitgreece/login/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.blue.visitgreece;
+package com.blue.visitgreece.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.blue.visitgreece.R;
 
 public class LoginActivity extends AppCompatActivity {
 
