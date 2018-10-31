4c4f470912c581df06d7806cbeee1c39b0cc420a
Tns <athbenos@gmail.com>
2018-10-31 20:35:38 +0200

    activity Homeview

--- app/src/main/java/com/blue/visitgreece/base/LoginActivity.java
+++ app/src/main/java/com/blue/visitgreece/base/LoginActivity.java
@@ -1,17 +1,16 @@
-package com.blue.visitgreece.login;
+package com.blue.visitgreece.base;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.blue.visitgreece.R;
-
-import com.blue.visitgreece.reviews.ReviewsFragment;
-
 import com.blue.visitgreece.base.HomeView;
+import com.blue.visitgreece.login.LoginFragment;
+import com.blue.visitgreece.login.LoginUI;
+import com.blue.visitgreece.reviews.ReviewsFragment;
 import com.blue.visitgreece.tourpackages.TourpackageUI;
 import com.blue.visitgreece.tourpackages.TourspackagesFragment;
 import com.blue.visitgreece.tours.ToursFragment;
-
 
 
 public class LoginActivity extends AppCompatActivity implements HomeView{
@@ -21,30 +20,24 @@ public class LoginActivity extends AppCompatActivity implements HomeView{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-
-        //temporary , must get delete when we merge packages and set proper fragments for app flow
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.login_root, new ReviewsFragment())
                 .commit();
-
-
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .add(R.id.login_root, new LoginFragment())
-//                .commit();
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.login_root, new TourspackagesFragment().newInstance(this))
-                .commit();
-
     }
 
 
+//    @Override
+//    public void addToursPackageFragment(LoginFragment login) {
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.login_root, new TourspackagesFragment().newInstance(login))
+//                .commit();
+//    }
+
     @Override
     public void addToursFragment(TourpackageUI tourpackageUI) {
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.login_root, ToursFragment.newInstance(tourpackageUI))
@@ -54,4 +47,3 @@ public class LoginActivity extends AppCompatActivity implements HomeView{
     }
 
 }
-
